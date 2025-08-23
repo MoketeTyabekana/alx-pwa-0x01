9a574e738959608f1dd495808a09cc027217dea6
Mokete <ematyabekana@gmail.com>
2025-08-23 21:29:53 +0200

    Integrate PWA support by updating Next.js configuration and exporting withPWA

--- alx-movie-app/next.config.ts
+++ alx-movie-app/next.config.ts
@@ -1,8 +1,21 @@
 import type { NextConfig } from "next";
 
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: 'public'
+});
+
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  images: {
+    domains: ['m.media-amazon.com'],
+  },
+  // Add your webpack config here if needed
 };
 
-export default nextConfig;
+export default withPWA({
+  ...nextConfig
+});
+
+// export default nextConfig; // Removed: Only one default export allowed
